2f829017ed8c0a2a17fd88bb05868b6714c38bfd
Manuel Araoz <manuelaraoz@gmail.com>
2013-11-05 10:20:12 -0300

    integrating emil design

---
+++ static/js/cufon-replace.js
@@ -0,0 +1,4 @@
+Cufon.replace('.table-2 th', { fontFamily: 'Kozuka Gothic Pro B', hover:true });
+Cufon.replace('.button, #contact-form .buttons a', { fontFamily: 'Bebas', hover:true });
+Cufon.replace('h3', { fontFamily: 'Six Caps', hover:true });
+Cufon.replace('.links-1 .text-1, .links-2 .text-1, .links-1 .text-2, .links-2 .text-2, .sf-menu li a, h4, h5, .list-1 li a', { fontFamily: 'Oswald', hover:true });
